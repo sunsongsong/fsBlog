d183319336fe085cf8b4ec8763c385c441606421
Song <805459487@qq.com>
2019-03-16 22:03:33 +0800

    Update AspectConfig.java

--- src/main/java/com/vicky/fsblog/config/AspectConfig.java
+++ src/main/java/com/vicky/fsblog/config/AspectConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
- * Spring AOP 的配置
+ * Spring AOP 的配置-
  *
  * @author James
  */
